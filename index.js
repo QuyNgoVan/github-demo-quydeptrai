5f86dfd6ec3a3141dd8f52bd2808f6e40d8e4ed4
Ngô Văn Qúy <edokawaconan4@gmail.com>
2020-09-07 00:40:36 +0700

    add index.js file

---
+++ index.js
@@ -0,0 +1,67 @@
+var readlineSync = require('readline-sync');
+var fs = require('fs');
+var students = [];
+function showMenu(){
+    console.log('1. Show all student');
+    console.log('2. Create a new student data');
+    console.log('3. Save and Exit');
+    var option = readlineSync.question('Nhap lua chon cua ban: >');
+    switch(option){
+        case '1':
+            showStudents();
+            showMenu();
+            break;
+        case'2':
+            showCreateStudent();
+           
+            showMenu();
+            break;
+        case '3':
+            saveAndExit();
+            break;
+        default:
+            console.log('Wrong option');
+            showMenu();
+            break;
+
+    }
+}
+
+function loadData(){
+    var fileContent = fs.readFileSync('./data.json');
+    students = JSON.parse(fileContent);
+
+}
+function showStudents(){
+    for( var student of students){
+        console.log(student.name, student.age );
+    
+    }
+
+}
+function showCreateStudent(){
+    var name = readlineSync.question('Name: ');
+    var age  = readlineSync.question('Age: ');
+    var student = {
+        name: name,
+        age: parseInt(age)
+
+    };
+    students.push(student);
+
+
+}
+function saveAndExit(){
+    var content = JSON.stringify(students);
+    console.log(content);
+    fs.writeFileSync('./data.json', content,{encoding: 'utf-8'});
+
+}
+function main() {
+    loadData();
+    
+    showMenu();
+    
+
+}
+main();
